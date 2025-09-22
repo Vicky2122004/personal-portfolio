63f44c2b0094b6d613b5c578ac3f93d46c5f59e5
Vicky2122004 <thangavelvicky21@gmail.com>
2025-09-22 20:32:23 +0530

    feat: Update resume download link to new ATS-friendly format

--- src/utils/downloadResume.ts
+++ src/utils/downloadResume.ts
@@ -1,7 +1,7 @@
 // utils/downloadResume.ts
 export const downloadResume = () => {
   const link = document.createElement("a");
-  link.href = "/assets/resume/resume.pdf";
+  link.href = "/assets/resume/vicky_new_ats_resume.pdf";
   link.download = "Vignesh_Resume.pdf";
   document.body.appendChild(link);
   link.click();
